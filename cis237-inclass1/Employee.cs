e662c5ec2b34b005ca7bfaf890a333f07b0881fe
David Barnes <dbarnes@kvcc.edu>
2019-09-10 17:26:02 -0400

    Add Employee class Add UserInterface class Update Program to use Employee and UserInterface.

---
+++ cis237-inclass1/Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237_inclass1
+{
+    class Employee
+    {
+        //***************************
+        // Variables / Backing fields
+        //***************************
+        private string _firstName;
+        private string _lastName;
+        private decimal _weeklySalary;
+
+        //****************************
+        // Properties
+        //****************************
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public decimal WeeklySalary
+        {
+            get { return _weeklySalary; }
+            set { _weeklySalary = value; }
+        }
+
+        //***************************
+        // Methods
+        //***************************
+        public string FirstAndLastName()
+        {
+            return _firstName + " " + _lastName;
+        }
+
+        public override string ToString()
+        {
+            // Fancy formatter.
+            return $"{_firstName} {_lastName} {_weeklySalary.ToString("C")}";
+        }
+
+        //***************************
+        // Constructor
+        //***************************
+        public Employee(
+            string FirstName,
+            string LastName,
+            decimal WeeklySalary
+        )
+        {
+            this._firstName = FirstName;
+            this._lastName = LastName;
+            this._weeklySalary = WeeklySalary;
+        }
+
+        public Employee()
+        {
+            // Do Nothing. Just added so we would have a default again.
+        }
+    }
+}
--- cis237-inclass1/Program.cs
+++ cis237-inclass1/Program.cs
@@ -10,6 +10,37 @@ namespace cis237_inclass1
     {
         static void Main(string[] args)
         {
+            // Make a new instance of the User Interface class
+            UserInterface ui = new UserInterface();
+
+            // Simply to show the default constructor works.
+            Employee myEmployee = new Employee();
+
+            // Make an actual employee with properties and output it.
+            Employee myOtherEmployee = new Employee(
+                "David",
+                "Barnes",
+                4500.87m
+            );
+
+
+            // Use the UI class to figure out what to do
+            // Get some input from the user
+            int choice = ui.GetUserInput();
+
+            // While the choice they selected is not 2, continue
+            while(choice != 2)
+            {
+                // See if the input they send is equal to 1.
+                if (choice == 1)
+                {
+                    // Output are one employee.
+                    ui.Output(myOtherEmployee.ToString());
+                }
+
+                //Re-prompt the user for input
+                choice = ui.GetUserInput();
+            }
         }
     }
 }
