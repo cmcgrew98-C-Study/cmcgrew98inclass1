1fa96ded2a96b5d97b2c96306dfa7510b04da3e8
David Barnes <dbarnes@kvcc.edu>
2019-09-12 17:17:43 -0400

    Add CSVProcessor class. Update Program to use the new CSVProcessor and output the array of employees.

---
+++ cis237-inclass1/CSVProcessor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace cis237_inclass1
+{
+    class CSVProcessor
+    {
+        public void ImportCsv(string pathToCSVFile, Employee[] employees)
+        {
+            // Declare the StreamReader
+            StreamReader streamReader = null;
+
+            try
+            {
+                // Declare a string for the line
+                string line;
+
+                // Make an instance of the StreamReader
+                streamReader = new StreamReader(pathToCSVFile);
+
+                // Initialize a counter for number of records
+                int counter = 0;
+
+                // While we are still reading a line from the file
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    // Process the line
+                    this.ProcessLine(line, employees, counter++);
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                // Output the exception and the stacktrace for the exception
+                Console.WriteLine("The path to the file is invalid.");
+                Console.WriteLine(e.ToString());
+                Console.WriteLine();
+                Console.WriteLine(e.StackTrace);
+                // How to throw an exception
+                throw new Exception("The path to the file is invalid.");
+            }
+            catch (Exception e)
+            {
+                // Output the exception and the stacktrace for the exception
+                Console.WriteLine(e.ToString());
+                Console.WriteLine();
+                Console.WriteLine(e.StackTrace);
+                // How to throw an existing exception
+                throw e;
+            }
+            finally
+            {
+                // If the stream reader was created, make sure it
+                // is closed before exiting the reader
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+            }
+        }
+
+        private void ProcessLine(string line, Employee[] employees, int index)
+        {
+            // Declare array of parts that will contain the results
+            // of splitting the read in string.
+            string[] parts = line.Split(',');
+
+            // Assign each part to a variable
+            string firstName = parts[0];
+            string lastName = parts[1];
+            decimal salary = decimal.Parse(parts[2]);
+
+            // Add a new employee to the array
+            employees[index] = new Employee(firstName, lastName, salary);
+        }
+    }
+}
--- cis237-inclass1/Program.cs
+++ cis237-inclass1/Program.cs
@@ -23,6 +23,35 @@ namespace cis237_inclass1
                 4500.87m
             );
 
+            // Let's make an array to hold a bunch of instances of
+            // the Employee class
+            Employee[] employees = new Employee[10];
+
+            // Let's add some employees to our array
+            //employees[0] = new Employee("David", "Barnes", 835.00m);
+            //employees[1] = new Employee("James", "Kirk", 453.0m);
+            //employees[2] = new Employee("Jean-Luc", "Picard", 563.0m);
+            //employees[3] = new Employee("Benjamin", "Sisko", 753.0m);
+            //employees[4] = new Employee("Kathryn", "Janeway", 953.0m);
+            //employees[5] = new Employee("Johnathan", "Archer", 643.0m);
+
+            // Make a string for the path to the csv file
+            string pathToCsv = "../../employees.csv";
+
+            // Create the instance of CSVProcessor
+            CSVProcessor csvProcessor = new CSVProcessor();
+
+            try
+            {
+                // Call the ImportCSV method sending over the path
+                // and the array to store the records in.
+                csvProcessor.ImportCsv(pathToCsv, employees);
+            }
+            catch (Exception)
+            {
+                // All the work in already in CSVProcessor. Could
+                // do more error messages if we wanted to.
+            }
 
             // Use the UI class to figure out what to do
             // Get some input from the user
@@ -34,8 +63,24 @@ namespace cis237_inclass1
                 // See if the input they send is equal to 1.
                 if (choice == 1)
                 {
-                    // Output are one employee.
-                    ui.Output(myOtherEmployee.ToString());
+                    // Create a string that can be concated to
+                    string outputString = "";
+
+                    // Print out the employees in the array
+                    // for (int i = 0; i < employees.Length; i++)
+                    foreach (Employee employee in employees)
+                    {
+                        // So long as NOT null, add it to the outputstring
+                        if (employee != null)
+                        {
+                            // Concat to the output string
+                            outputString += employee.ToString() +
+                                Environment.NewLine;
+                        }
+                    }
+
+                    // Use the UI class to print out the string
+                    ui.Output(outputString);
                 }
 
                 //Re-prompt the user for input
